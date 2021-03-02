e60f6749cb28d9b8d5b7538c21c2d10ed0b8dae6
Jory <46983170+Cambro320@users.noreply.github.com>
2021-03-01 18:51:27 -0600

    added sticky navbar

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const alert = document.getElementById('alert');
+const navbar = document.getElementById('navbar');
+const sticky = navbar.offsetTop;
+
+window.onscroll = function () {
+  stickyNav()
+};
 
 alert.addEventListener('click', toggleHidden);
 
 function toggleHidden() {
   // toggle hidden 
   alert.classList.toggle('hidden');
+}
+
+function stickyNav() {
+  if (window.pageYOffset > sticky + 1) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
