287e9051834f19a4a9562e87f229455848c20614
Jory <46983170+Cambro320@users.noreply.github.com>
2021-03-16 10:45:56 -0500

    made navbar cart dynamic to show updated items in shopping cart

--- script.js
+++ script.js
@@ -2,11 +2,39 @@ const alert = document.getElementById('alert');
 const navbar = document.getElementById('navbar');
 const sticky = navbar.offsetTop;
 
+
+
+
+
+//click to remove free shipping alert banner
 alert.addEventListener('click', toggleHidden);
 
+//create a sticky nav once the user starts to scroll down
 window.onscroll = function () {
   stickyNav()
 };
+
+updateNavbarCartIconTotal()
+//update navbar shopping cart icon to reflect total items in shopping cart 
+function updateNavbarCartIconTotal() {
+  const totalCartItems = document.getElementById('total-cart-items');
+  const updatedTotal = updateTotalShoppingCartItems().length.toString();
+  console.log(updatedTotal);
+  totalCartItems.innerText = updatedTotal;
+}
+
+updateTotalShoppingCartItems()
+//get total number of items in the shopping cart
+function updateTotalShoppingCartItems() {
+  const totalShoppingCartItems = document.getElementsByClassName('shopping-cart-item');
+  return totalShoppingCartItems
+}
+
+
+
+
+
+
 
 function toggleHidden() {
   alert.classList.toggle('hidden');
