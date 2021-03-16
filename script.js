2f5ceef20421dae634fdddf6b16fba2eb5011d23
Jory <46983170+Cambro320@users.noreply.github.com>
2021-03-16 15:46:45 -0500

    added open/close functionality to shopping cart

--- script.js
+++ script.js
@@ -1,36 +1,47 @@
 const alert = document.getElementById('alert');
 const navbar = document.getElementById('navbar');
 const sticky = navbar.offsetTop;
+const navbarCartIcon = document.querySelector('.fa-shopping-bag');
+const shoppingCart = document.querySelector('.shopping-cart-container');
+const closeCartDiv = document.querySelector('.close-cart-window');
 const removeCartItemIcons = document.querySelectorAll('.fa-trash');
 
 //event listener for click to remove free shipping alert banner
-alert.addEventListener('click', toggleHidden);
+alert.addEventListener('click', () => {
+  alert.classList.toggle('hidden');
+});
 
 //create a sticky nav once the user starts to scroll down
 window.onscroll = function () {
   stickyNav()
 };
 
+//event listener on navbar shopping cart icon click to show/hide cart
+navbarCartIcon.addEventListener('click', closeCart);
+
+//event listener on shopping cart close div to close it
+closeCartDiv.addEventListener('click', closeCart);
+
 //event listeners for click for each remove cart item trash icons
 removeCartItemIcons.forEach(el =>
   el.addEventListener('click', removeShoppingCartItem));
 
-
-
 updateNavbarCartIconTotal()
 updateTotalShoppingCartItems()
 
+function closeCart() {
+  shoppingCart.classList.toggle('hidden');
+}
 
 function stickyNav() {
   if (window.pageYOffset > sticky + 1) {
     navbar.classList.add("sticky");
+    shoppingCart.classList.add("sticky");
+    shoppingCart.style.margin = "3.25rem 0 2rem"
   } else {
     navbar.classList.remove("sticky");
+    shoppingCart.classList.remove("sticky");
   }
-}
-
-function toggleHidden() {
-  alert.classList.toggle('hidden');
 }
 
 //update navbar shopping cart icon to reflect total items in shopping cart 
