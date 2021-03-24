38427a52f956cfec613793cd204f1c8b4c321b52
Jory <46983170+Cambro320@users.noreply.github.com>
2021-03-24 15:50:40 -0500

    fixed a qty-selector bug

--- script.js
+++ script.js
@@ -139,12 +139,12 @@ function addItemToCart(itemImage, itemName, itemPrice) {
           <option value="2">2</option>
           <option value="3">3</option>
           <option value="4">4</option>
-          <option value="1">5</option>
-          <option value="2">6</option>
-          <option value="3">7</option>
-          <option value="4">8</option>
-          <option value="3">9</option>
-          <option value="4">10</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
         </select>
       </div>
     </div>
