e193ae892993146ceade18b1957e3f71f975dea4
Jory <46983170+Cambro320@users.noreply.github.com>
2021-03-24 15:45:38 -0500

    completed shopping cart functionaility, qty updates dynamically

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const sticky = navbar.offsetTop;
 const navbarCartIcon = document.querySelector('.fa-shopping-bag');
 const shoppingCart = document.querySelector('.shopping-cart');
 const closeCartDiv = document.querySelector('.close-cart-window');
+const quantity = document.querySelectorAll('.qty');
 const removeCartItemIcons = document.querySelectorAll('.fa-trash');
 const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
 const completePurchase = document.getElementById('checkout-btn');
+
 
 //event listener for click to remove free shipping alert banner
 shippingAlert.addEventListener('click', () => {
@@ -23,6 +25,10 @@ navbarCartIcon.addEventListener('click', toggleCart);
 
 //event listener on shopping cart 'close' div to close it
 closeCartDiv.addEventListener('click', toggleCart);
+
+//event listeners on each cart item quanity value
+quantity.forEach(el =>
+  el.addEventListener('change', updateShoppingCartTotal));
 
 //event listeners on each cart item trash icons for removal
 removeCartItemIcons.forEach(el =>
@@ -63,7 +69,6 @@ function updateNavbarCartIconTotal() {
   //update navbar shopping cart icon to reflect total items in shopping cart
   const totalCartItems = document.getElementById('total-cart-items');
   const updatedTotal = updateTotalShoppingCartItems().length.toString();
-
   //toggle hidden on cart icon total, avoid showing a zero
   //if cart is empty, when you add to cart, unhide icon
   if (updatedTotal == 0) {
@@ -83,20 +88,23 @@ function updateTotalShoppingCartItems() {
 function addClickedItemToCart(event) {
   //update navbar cart icon and total cart items
   //get button clicked item name, image, and price
-  //grab clicked item container/shopping-cart-item, query classes inside it
-  //see if item is already in the cart, add reject message if so
-
+  //grab clicked item .shopping-cart-item, query classes inside it
+  //see if item is already in the cart, alert message if so
   updateNavbarCartIconTotal()
   updateTotalShoppingCartItems()
   updateShoppingCartTotal()
 
   const clickedItemContainer = event.target.parentElement
-  const itemImage = clickedItemContainer.getElementsByClassName('item-img')[0].src
-  const itemName = clickedItemContainer.getElementsByClassName('item-name')[0].innerText
-  const itemPrice = clickedItemContainer.getElementsByClassName('item-price')[0].innerText;
+  const itemImage = clickedItemContainer.getElementsByClassName(
+    'item-img')[0].src
+  const itemName = clickedItemContainer.getElementsByClassName(
+    'item-name')[0].innerText;
+  const itemPrice = clickedItemContainer.getElementsByClassName(
+    'item-price')[0].innerText;
 
   //see if current items names in cart match clicked item name
-  const currentCartItemNames = document.getElementsByClassName('cart-item-name');
+  const currentCartItemNames = document.getElementsByClassName(
+    'cart-item-name');
 
   for (let i = 0; i < currentCartItemNames.length; i++) {
     if (currentCartItemNames[i].innerText.trim() == itemName) {
@@ -105,7 +113,6 @@ function addClickedItemToCart(event) {
     }
   }
   addItemToCart(itemImage, itemName, itemPrice)
-
 }
 
 function addItemToCart(itemImage, itemName, itemPrice) {
@@ -113,8 +120,9 @@ function addItemToCart(itemImage, itemName, itemPrice) {
   //get shopping cart item div to append new cart item
   const newCartItemDiv = document.createElement('div');
   newCartItemDiv.classList.add('shopping-cart-item');
-  document.getElementsByClassName('shopping-cart-items')[0].append(newCartItemDiv);
-  const testDiv = `    
+  document.getElementsByClassName('shopping-cart-items')[0].append(
+    newCartItemDiv);
+  const cartItemTemplate = `    
   <div class="cart-item-img">
     <img class="cart-item-img" src="${itemImage}">
   </div>
@@ -148,11 +156,16 @@ function addItemToCart(itemImage, itemName, itemPrice) {
       <i class="fas fa-trash"></i>
     </div>
   </div>`
-  newCartItemDiv.innerHTML = testDiv
+  newCartItemDiv.innerHTML = cartItemTemplate
+
   updateTotalShoppingCartItems();
   updateNavbarCartIconTotal();
   updateShoppingCartTotal()
-  newCartItemDiv.getElementsByClassName('fa-trash')[0].addEventListener('click', removeShoppingCartItem);
+
+  newCartItemDiv.getElementsByClassName('fa-trash')[0].addEventListener(
+    'click', removeShoppingCartItem);
+  newCartItemDiv.getElementsByClassName('qty')[0].addEventListener(
+    'change', updateShoppingCartTotal);
 }
 
 function removeShoppingCartItem(event) {
@@ -161,27 +174,33 @@ function removeShoppingCartItem(event) {
   updateTotalShoppingCartItems();
   updateNavbarCartIconTotal();
   updateShoppingCartTotal()
-  console.log()
 }
 
 function updateShoppingCartTotal() {
-  //grab each item's price, sum it
-  const cartItemPrices = document.getElementsByClassName('cart-item-price');
-  let totalArray = [];
-  for (let i = 0; i < cartItemPrices.length; i++) {
-    totalArray.push(parseFloat(cartItemPrices[i].innerText.replace('$', '')));
+  //loop through qty-price-remove-container, grab each item's price multiplied its quantity, sum all items
+  const qtyPriceContainer = document.getElementsByClassName(
+    'qty-price-remove-container');
+  let newTotal = 0;
+
+  for (let i = 0; i < qtyPriceContainer.length; i++) {
+    //get item qty, get item price, multiply each, add to total array
+    const cartItem = qtyPriceContainer[i]
+    const quantity = cartItem.getElementsByClassName('qty')[0].value
+    const price = cartItem.getElementsByClassName(
+      'cart-item-price')[0].innerText.replace('$', '')
+    newTotal = newTotal + (quantity * parseFloat(price))
+
   }
-  const cartTotal = totalArray.reduce((sum, currentItem) => {
-    return currentItem + sum;
-  }, 0);
+
 
   // insert total into shopping cart total
-  document.getElementById('cart-total-price').innerText = "$" + cartTotal;
+  document.getElementById('cart-total-price').innerText = "$" + newTotal;
 }
 
 function clearCartAfterPurchase() {
   //get all shopping cart items, remove them
-  const currentItems = document.getElementsByClassName('shopping-cart-items')[0];
+  const currentItems = document.getElementsByClassName(
+    'shopping-cart-items')[0];
 
   if (updateTotalShoppingCartItems().length > 0) {
     alert('Thank you for your purchase!')
